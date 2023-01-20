4aa71f791095f65478d6fbd984ccc2415b11f8d3
Ben Courliss <bencourliss@ibm.com>
2023-01-20 14:54:40 -0500

    Exporting HZN_FDO_API_URL
    Signed-off-by: Ben Courliss <bencourliss@ibm.com>

--- docker/start-fdo-owner-service.sh
+++ docker/start-fdo-owner-service.sh
@@ -21,7 +21,7 @@ ownerPort=${HZN_FDO_SVC_URL:-$ownerPortDefault}
 ownerExternalPort=${FDO_OWNER_EXTERNAL_PORT:-$ownerPort}
 rvPort=${FDO_RV_PORT:-$rvPortDefault}
 dbPort=${FDO_DB_PORT:-5432}
-HZN_FDO_API_URL="http://$FDO_OPS_URL"
+export HZN_FDO_API_URL="http://$FDO_OPS_URL"
 
 #VERBOSE='true'   # let it be set by the container provisioner
 FDO_SUPPORT_RELEASE=${FDO_SUPPORT_RELEASE:-https://github.com/secure-device-onboard/release-fidoiot/releases/download/v1.1.4}
